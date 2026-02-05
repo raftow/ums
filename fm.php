d56ed458d67c023ed2800971e2bdcd04f760b36e
raftow <rboubaker@tvtc.gov.sa>
2026-02-05 22:15:56 +0300

    %1

--- fm.php
+++ fm.php
@@ -83,37 +83,38 @@ if ($r == "control") {
   }
 } elseif ($r) {
 
-  list($role_item_display, $menu_folder, $role_data) = UmsManager::getRoleDetails($a, $r, $lang);
-  // die("rafik see menu_folder = ".var_export($menu_folder,true));
-  $role_item_display = AfwReplacement::trans_replace($role_item_display, $module, $lang);
-  /**
-   * @var array $menu_folder
-   */
-  if ($role_item_display and $menu_folder) {
-    $out_scr .= "<h3>" . $role_item_display . "</h3>";
-    $count_all = count($menu_folder["items"]) + count($menu_folder["sub-folders"]);
-    if ($count_all > 20) $s12 = "s3";
-    elseif ($count_all > 3) $s12 = "s6";
-    else $s12 = "s12";
-    foreach ($menu_folder["items"] as $menu_folder_item_id => $menu_folder_item) {
-      $menu_item_id = $menu_folder_item["id"];
-      $menu_item_level = $menu_folder_item["level"];
-      $menu_item_title = $menu_folder_item["menu_name_$lang"];
-      if (!$menu_item_title) $menu_item_title = $menu_folder_item["menu_name"];
-      $menu_item_title = AfwReplacement::trans_replace($menu_item_title, $module, $lang);
+  if ($objme->hasRole($module, $r)) {
+    list($role_item_display, $menu_folder, $role_data) = UmsManager::getRoleDetails($a, $r, $lang);
+    // die("rafik see menu_folder = ".var_export($menu_folder,true));
+    $role_item_display = AfwReplacement::trans_replace($role_item_display, $module, $lang);
+    /**
+     * @var array $menu_folder
+     */
+    if ($role_item_display and $menu_folder) {
+      $out_scr .= "<h3>" . $role_item_display . "</h3>";
+      $count_all = count($menu_folder["items"]) + count($menu_folder["sub-folders"]);
+      if ($count_all > 20) $s12 = "s3";
+      elseif ($count_all > 3) $s12 = "s6";
+      else $s12 = "s12";
+      foreach ($menu_folder["items"] as $menu_folder_item_id => $menu_folder_item) {
+        $menu_item_id = $menu_folder_item["id"];
+        $menu_item_level = $menu_folder_item["level"];
+        $menu_item_title = $menu_folder_item["menu_name_$lang"];
+        if (!$menu_item_title) $menu_item_title = $menu_folder_item["menu_name"];
+        $menu_item_title = AfwReplacement::trans_replace($menu_item_title, $module, $lang);
 
-      if ($menu_item_level >= $myLevel) {
-        $out_scr .= "<!-- OK => $menu_item_title BFID=$menu_item_id BFL=$menu_item_level >= UHL=$myLevel (User=$myName ID=$myId)-->";
+        if ($menu_item_level >= $myLevel) {
+          $out_scr .= "<!-- OK => $menu_item_title BFID=$menu_item_id BFL=$menu_item_level >= UHL=$myLevel (User=$myName ID=$myId)-->";
 
-        $menu_item_icon = $menu_folder_item["icon"];
+          $menu_item_icon = $menu_folder_item["icon"];
 
-        if ((!$menu_item_icon) and $menu_icons_arr[$menu_item_id]) $menu_item_icon = $menu_icons_arr[$menu_item_id];
-        if (!$menu_item_icon) $menu_item_icon = "globe icon-$menu_item_id";
+          if ((!$menu_item_icon) and $menu_icons_arr[$menu_item_id]) $menu_item_icon = $menu_icons_arr[$menu_item_id];
+          if (!$menu_item_icon) $menu_item_icon = "globe icon-$menu_item_id";
 
-        $menu_item_page = $menu_folder_item["page"] . "&r=$r";
-        $menu_item_css = $menu_folder_item["css"];
+          $menu_item_page = $menu_folder_item["page"] . "&r=$r";
+          $menu_item_css = $menu_folder_item["css"];
 
-        $out_scr .= "<div id='menu-item-$menu_item_id' class='$menu_item_css hzm-menu-item hzm3-col l3 m3 $s12'>
+          $out_scr .= "<div id='menu-item-$menu_item_id' class='$menu_item_css hzm-menu-item hzm3-col l3 m3 $s12'>
                                 <a class='action_lourde hzm3-button hzm3-light-grey hzm3-block' href='$menu_item_page' style='white-space:nowrap;text-decoration:none;margin-top:1px;margin-bottom:1px'>
                                     <div class=\"hzm-width-100 hzm-text-center hzm_margin_bottom \">
                                       <div class=\"hzm-vertical-align hzm-container-center hzm-custom hzm-custom-icon-container only-border border-primary\">
@@ -123,21 +124,21 @@ if ($r == "control") {
                                     <div class='hzm4-title'>$menu_item_title</div>
                                 </a>
                              </div>";
-      } else {
-        $out_scr .= "<!-- NOT-OK => $menu_item_title BFID=$menu_item_id BFL=$menu_item_level < UHL=$myLevel (User=$myName ID=$myId)-->";
+        } else {
+          $out_scr .= "<!-- NOT-OK => $menu_item_title BFID=$menu_item_id BFL=$menu_item_level < UHL=$myLevel (User=$myName ID=$myId)-->";
+        }
       }
-    }
-    foreach ($menu_folder["sub-folders"] as $menu_sub_folder_id => $menu_sub_folder) {
-      if (($iamAdmin) or (!$menu_sub_folder["need_admin"])) {
-        $menu_folder_title = $menu_sub_folder["menu_name"];
+      foreach ($menu_folder["sub-folders"] as $menu_sub_folder_id => $menu_sub_folder) {
+        if (($iamAdmin) or (!$menu_sub_folder["need_admin"])) {
+          $menu_folder_title = $menu_sub_folder["menu_name"];
 
-        $menu_folder_title = AfwReplacement::trans_replace($menu_folder_title, $module, $lang);
+          $menu_folder_title = AfwReplacement::trans_replace($menu_folder_title, $module, $lang);
 
-        $menu_folder_id = $menu_sub_folder["id"];
-        $menu_folder_page = $menu_sub_folder["page"];
-        $menu_folder_css = $menu_sub_folder["css"];
+          $menu_folder_id = $menu_sub_folder["id"];
+          $menu_folder_page = $menu_sub_folder["page"];
+          $menu_folder_css = $menu_sub_folder["css"];
 
-        $out_scr .= "<div id='menu-folder-$menu_sub_folder_id' class='$menu_folder_css hzm-menu-folder hzm3-col l3 m3 s12'>
+          $out_scr .= "<div id='menu-folder-$menu_sub_folder_id' class='$menu_folder_css hzm-menu-folder hzm3-col l3 m3 s12'>
                                          <a class='hzm3-button hzm3-light-grey hzm3-block' href='$menu_folder_page' style='white-space:nowrap;text-decoration:none;margin-top:1px;margin-bottom:1px'>
                                             <div class=\"hzm-width-100 hzm-text-center hzm_margin_bottom \">
                                               <div class=\"hzm-vertical-align hzm-container-center hzm-custom hzm-custom-icon-container only-border border-primary\">
@@ -147,12 +148,16 @@ if ($r == "control") {
                                             <div class='hzm4-title'>$menu_folder_title</div>
                                           </a>
                                      </div>";
+        }
       }
+    } else {
+      $out_scr .= "role details not found for role_id=$r and app_id=$a ";
+      $out_scr .= " <!-- role_data = " . var_export($role_data, true) . " -->";
+      // $out_scr .=" <!-- role_info = ".var_export($role_info,true)." -->";
     }
-  } else {
-    $out_scr .= "role details not found for role_id=$r and app_id=$a ";
-    $out_scr .= " <!-- role_data = " . var_export($role_data, true) . " -->";
-    // $out_scr .=" <!-- role_info = ".var_export($role_info,true)." -->";
+  }
+  else {
+      $out_scr .= "You don't have this previlege";
   }
 }
 
