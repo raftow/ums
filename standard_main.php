80c137da184c1e3d0903875fd3edca2cb602bb00
raftow <rboubaker@tvtc.gov.sa>
2026-03-09 16:47:24 +0300

    %1

---
+++ standard_main.php
@@ -0,0 +1,21 @@
+<?php
+// IMPORTANT !!!!!!!!
+// keep $file_dir_name variable not defined here but coming from each module's main.php file
+include_once ("$file_dir_name/ini.php");
+include_once ("$file_dir_name/module_config.php"); 
+require("$file_dir_name/../lib/afw/afw_main_page.php"); 
+if($_REQUEST["Main_Page"])
+{
+    $Main_Page = $_REQUEST["Main_Page"];
+}
+else
+{
+    $Main_Page = "home.php";
+}
+
+$table = null;
+if(isset($_REQUEST["cl"])) $table = AfwStringHelper::classToTable($_REQUEST["cl"]);
+if(!$table) $table = "all";
+$options = AfwMainPage::getDefaultOptions($Main_Page, $MODULE, $table);
+
+AfwMainPage::echoMainPage($MODULE, $Main_Page, $file_dir_name, $options);
