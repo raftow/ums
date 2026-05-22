49f3025ba6c748023f66d65b6c2258f97178f17f
rafiq boubaker <rafik.towati@gmail.com>
2026-05-22 22:40:49 +0300

    some important improvement

--- models/auser.php
+++ models/auser.php
@@ -1999,9 +1999,9 @@ class Auser extends UmsObject implements AfwFrontEndUser
                                                         $mau_info_item[$bfCode0][$roleId] = true;
                                                 }
 
-                                                if (($moduleId == 1282) and ($roleId == 408)) {
-                                                        die("rafik dbg 260521019 bfList = " . var_export($bfList, true) . " mau_info_item = " . var_export($mau_info_item, true));
-                                                }
+                                                /*if (($moduleId == 1282) and ($roleId == 408)) {
+                                                        die("rafik dbg 260521019 bfList = " . var_export($bfList, true) . "<br> mau_info_item = " . var_export($mau_info_item, true));
+                                                }*/
                                         }
                                 }
 
