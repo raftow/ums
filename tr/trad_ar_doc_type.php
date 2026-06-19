ae9f443ccd338db40e8d761a8805b65579fcdc79
rafiq boubaker <rafik.towati@gmail.com>
2026-06-19 20:00:03 +0300

    some important improvement

---
+++ tr/trad_ar_doc_type.php
@@ -0,0 +1,6 @@
+<?php
+$trad["doc_type"]["doctype.single"] = "نوع مستند";
+$trad["doc_type"]["doctype.new"] = "جديد";
+$trad["doc_type"]["doc_type"] = "أنواع المستندات";
+$trad["doc_type"]["titre"] = "الوصف";
+$trad["doc_type"]["valid_ext"] = "قائمة امتدادات الملفات المسموح بها";
