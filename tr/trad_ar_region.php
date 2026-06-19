2df07a6a1aea5560f135e812989cb9863358a3d4
rafiq boubaker <rafik.towati@gmail.com>
2026-06-19 22:47:45 +0300

    some important improvement

---
+++ tr/trad_ar_region.php
@@ -0,0 +1,6 @@
+<?php
+	$trad["region"]["region.single"] = "منطقة";
+	$trad["region"]["region.new"] = "جديد(ة)";
+	$trad["region"]["region"] = "المناطق";
+	$trad["region"]["region_name"] = "مسمى المنطقة";
+?>
