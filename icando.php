6b53f6580ce65fbdc048113efd3b1cb7a212e513
rafiq boubaker <rafik.towati@gmail.com>
2026-07-15 20:12:18 +0300

    some important improvement

---
+++ icando.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @var string $lang
+ * @var string $out_scr
+ */
+$file_dir_name = dirname(__FILE__);
+
+require_once("$file_dir_name/../config/global_config.php");
+
+
+
+$objme = AfwSession::getUserConnected();
+$myEmplId = $objme->getEmployeeId();
+
+
+if (!$lang) $lang = AfwLanguageHelper::getGlobalLanguage();
+if (!$lang) $lang = "ar";
+$module = $_GET["md"];
+$afwClass = $_GET["cl"];
+$id = $_GET["id"];
+if (!$module) die("md param is required");
+AfwAutoLoader::addModule($module);
+if (!$afwClass) die("cl param is required");
+
+
+if ($id) $object = $afwClass::loadById($id);
+else $object = new $afwClass();
+
+AfwUmsPagHelper::userCanDoOperationOnObject($object, $auser, 'delete');
+
+$out_scr .= $objme->getICanDoLog();
--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 $direct_dir_name = dirname(__FILE__);
 include("$direct_dir_name/ums_start.php");
-// $objme = AfwSession::getUserConnected();
-
-$Main_Page = "home.php";
-$file_dir_name = dirname(__FILE__);
-$MODULE = $My_Module = "ums";
-$options = [];
-$options["dashboard-stats"] = true;
-$options["chart-js"] = true;
-CmsMainPage::echoMainPage($My_Module, $Main_Page, $file_dir_name);
+$icando = $_GET["icando"];
+if ($icando) {
+    $Main_Page = "icando.php";
+    $file_dir_name = dirname(__FILE__);
+    $MODULE = $My_Module = "ums";
+    $options = [];
+    CmsMainPage::echoMainPage($My_Module, $Main_Page, $file_dir_name);
+} else {
+    $Main_Page = "home.php";
+    $file_dir_name = dirname(__FILE__);
+    $MODULE = $My_Module = "ums";
+    $options = [];
+    $options["dashboard-stats"] = true;
+    $options["chart-js"] = true;
+    CmsMainPage::echoMainPage($My_Module, $Main_Page, $file_dir_name);
+}
